fc324244c57426aea19ddae9338428ab351e2d38
annt <annt@gft.com>
2022-11-24 12:54:33 +0000

    Done with for loop

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,19 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public double myPow(double x, int n){
+
+        double result = x;
+
+        for (int i = 0; i<n - 1; i++){
+
+            result *= x;
+        }
+
+        return result;
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,41 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+    double x, result;
+    int n;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void TwoRaisedToThePowerTen(){
+
+        x = 2.0;
+        n = 10;
+        result = 1024.0;
+
+        assertEquals(result, solution.myPow(x,n));
+
+    }
+
+    @Test
+    public void ThreePointFiveRaisedToThePowerThree(){
+
+        x = 3.5;
+        n = 3;
+        result = 42.875;
+
+        assertEquals(result, solution.myPow(x,n));
+
+    }
 }
